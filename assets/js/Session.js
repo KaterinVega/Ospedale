af02a55a8a3561394f6eb96e174e5cf34f048b25
KaterinVega <80361480+KaterinVega@users.noreply.github.com>
2022-08-27 15:09:07 -0500

    27/08/2022

--- assets/js/Session.js
+++ assets/js/Session.js
@@ -3,3 +3,18 @@ import { Account } from "./API/class/Account.js";
 $(document).ready(() => {
     
 });
+
+$("#btn_login").click(function () {
+
+    var documento = $("#txt_documento").val();
+    var contrasena = $("#txt_contrasena").val();
+
+    let account = new AccountModel("katerin@gmail.com", contrasena, "Administrador", documento)
+
+    account.add().then((r) => {
+        if (r == "ok") {
+             alert("Cuenta creada") ;
+        } 
+   }) ;
+
+  });
