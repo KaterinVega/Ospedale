307bd78e923ed46c3fc3ba100b3b1023196ffc72
KaterinVega <80361480+KaterinVega@users.noreply.github.com>
2022-08-27 17:09:22 -0500

    27/08/2022

---
+++ views/subinicio/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="./views/subinicio/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Ospedale | Acceso</title>
+</head>
+<body>
+<div class="dospartes">
+            <div class="fonoscu">
+                <header>
+                    <h3 class="menuss">Menu</h3>
+                    
+                </header>
+                <div class="arriba">
+                    <div class="form-group botoness">
+                    <a href="../especialistas/index.php" class="ov-btn-slide-right">TERCEROS ASISTENCIALES</a>
+                    </div>
+
+                    <div class="form-group botoness">
+                    <a href="alianzas.php" class="ov-btn-slide-right">TERCEROS ADMINISTRATIVOS</a>
+                    </div>
+                    
+                    
+                    <div class="form-group botoness">
+                    <a href="usuarios.php" class="ov-btn-slide-right">USUARIOS</a>
+                    </div>
+                    
+                </div>
+                <div class="abajo">
+                    <div class="salir">
+                        <div class="bots">
+                        <a class="ov-btn-slide-right-salir"href="index.php">SALIR</a>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+            </div>
+
+            <div class="fondosub">
+                
+            </div>
+        </div>
+</body>       
+
